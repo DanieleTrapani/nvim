aa717c8de78c02ad25f8c92a3e084257d4d43edc
Daniele Trapani <daniele.trapani96@gmail.com>
2023-12-28 20:18:59 -0500

    added keybind for NoiceDismiss

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -31,6 +31,8 @@ return {
         inc_rename = false,           -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false,       -- add a border to hover docs and signature help
       },
+      -- Dismiss Noice Messages
+      vim.keymap.set('n', '<leader>m', vim.cmd.NoiceDismiss, { desc = 'Dismiss messages' }),
     }
   end,
 }
