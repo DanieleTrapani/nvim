fa657c97eb2e6c5357d524b81597613426dcec6b
Daniele Trapani <daniele.trapani96@gmail.com>
2024-07-31 14:55:55 +0200

    added mini.move

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -35,4 +35,18 @@ return {
   },
 
   'tpope/vim-surround',
+
+  {
+    'echasnovski/mini.move',
+    version = '*',
+    config = function()
+      require('mini.move').setup {
+        mappings = {
+
+          line_down = '<M-Down>',
+          line_up = '<M-Up>',
+        },
+      }
+    end,
+  },
 }
